0cb80d814fb3fcf3781c42e42d03ece01b88b2a9
Bill Fraser <bill@wfraser.dev>
2023-12-04 11:45:56 -0800

    split things up a little

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use heck::{
     ToKebabCase, ToLowerCamelCase, ToPascalCase, ToShoutySnakeCase, ToSnakeCase, ToUpperCamelCase,
 };
-use proc_macro2::TokenStream;
+use proc_macro::TokenStream;
 use quote::ToTokens;
 use syn::parse::Parser;
 use syn::punctuated::Punctuated;
 use syn::spanned::Spanned;
 use syn::{
-    parse_macro_input, DataStruct, DeriveInput, Error, Expr, ExprAssign, ExprLit, ExprPath, Lit,
-    LitBool, Token,
+    parse_macro_input, Data, DataStruct, DeriveInput, Error, Expr, ExprAssign, ExprLit, ExprPath,
+    Field, Lit, LitBool, Token,
 };
 
 #[derive(Debug, Default)]
@@ -21,54 +21,42 @@ pub fn clap_wrapper(
     main_attr: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
+    let mut input = parse_macro_input!(input as DeriveInput);
+    if let Err(e) = apply(main_attr, &mut input) {
+        return e.to_compile_error().into();
+    }
+
+    input.to_token_stream().into()
+}
+
+fn apply(main_attr: TokenStream, input: &mut DeriveInput) -> syn::Result<()> {
     let mut opts = Options::default();
     let attr_exprs =
-        match Punctuated::<ExprAssign, Token![,]>::parse_terminated.parse(main_attr.clone()) {
-            Ok(exprs) => exprs,
-            Err(e) => return e.to_compile_error().into(),
-        };
+        Punctuated::<ExprAssign, Token![,]>::parse_terminated.parse(main_attr.clone())?;
     for expr in &attr_exprs {
         let key = expr.left.to_token_stream().to_string();
         match key.as_str() {
             "prefix" => {
-                let prefix = match expr_literal(&expr.right) {
-                    Ok(s) => s,
-                    Err(e) => return e.to_compile_error().into(),
-                };
-                opts.prefix = Some(prefix);
+                opts.prefix = Some(expr_str_lit(&expr.right)?);
             }
-            _ => {
-                return Error::new_spanned(&expr.left, "unrecognized attribute")
-                    .to_compile_error()
-                    .into()
-            }
+            _ => return Err(Error::new_spanned(&expr.left, "unrecognized attribute")),
         }
     }
 
-    let mut input: DeriveInput = parse_macro_input!(input);
-
-    if let Ok(false) = any_clap_here(&input) {
-        return Error::new_spanned(
+    if let Ok(false) = any_clap_here(input) {
+        return Err(Error::new_spanned(
             attr_exprs,
-            "this attribute needs to be put before any #[derive(Parser)]?",
-        )
-        .into_compile_error()
-        .into();
+            "this attribute needs to be put before any #[derive(clap::Parser)]",
+        ));
     }
 
     if let Some(prefix) = opts.prefix {
-        if let Err(e) = add_prefix_to_everything(&prefix, &mut input) {
-            return e.into_compile_error().into();
-        }
+        add_prefix_to_everything(&prefix, input)?;
     }
 
-    if let Err(e) = add_better_boolean_attrs(&mut input) {
-        return e.into_compile_error().into();
-    }
+    add_better_boolean_attrs(input)?;
 
-    let mut ts = TokenStream::new();
-    input.to_tokens(&mut ts);
-    ts.into()
+    Ok(())
 }
 
 fn clap_to_lower(s: &str) -> String {
@@ -91,152 +79,87 @@ fn any_clap_here(input: &DeriveInput) -> syn::Result<bool> {
     }
     Ok(false)
 }
-
 fn add_better_boolean_attrs(input: &mut DeriveInput) -> syn::Result<()> {
     match &mut input.data {
-        syn::Data::Struct(DataStruct { fields, .. }) => {
+        Data::Struct(DataStruct { fields, .. }) => {
             for field in fields {
                 if field.ty.to_token_stream().to_string() == "bool" {
-                    // look for 'default_value' or 'default_value_t'
-                    let mut default = None;
-                    for attr in &field.attrs {
-                        if let syn::Meta::List(list) = &attr.meta {
-                            if list.path.is_ident("arg") {
-                                let exprs = Punctuated::<Expr, Token![,]>::parse_terminated
-                                    .parse2(list.tokens.clone())?;
-                                for expr in exprs {
-                                    match expr {
-                                        Expr::Assign(expr) => {
-                                            default = Some(
-                                                match expr
-                                                    .left
-                                                    .to_token_stream()
-                                                    .to_string()
-                                                    .as_str()
-                                                {
-                                                    "default_value" => {
-                                                        expr_literal(&expr.right)? == "true"
-                                                    }
-                                                    "default_value_t" => {
-                                                        match expr.right.as_ref() {
-                                                            Expr::Lit(ExprLit {
-                                                                lit:
-                                                                    Lit::Bool(LitBool { value, .. }),
-                                                                ..
-                                                            }) => *value,
-                                                            _ => {
-                                                                return Err(Error::new_spanned(
-                                                                    expr.right,
-                                                                    "expected a bool literal",
-                                                                ))
-                                                            }
-                                                        }
-                                                    }
-                                                    _ => continue,
-                                                },
-                                            );
-                                        }
-                                        Expr::Call(call) => {
-                                            // default_value_t isn't a function
-                                            if call.func.to_token_stream().to_string()
-                                                == "default_value"
-                                            {
-                                                let arg = match call.args.first() {
-                                                    Some(arg) => arg,
-                                                    None => {
-                                                        return Err(Error::new_spanned(
-                                                            call,
-                                                            "expected a single arg",
-                                                        ))
-                                                    }
-                                                };
-                                                default = Some(expr_literal(arg)? == "true");
-                                            }
-                                        }
-                                        _ => (),
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    let default = get_bool_default(field)?;
 
-                    // Insert at the front, so later (i.e. manually-specified) attributes want to
-                    // override it, they can.
-                    field.attrs.insert(0, syn::parse_quote! {
-                        #[arg(num_args(0..=1), require_equals(true), action=clap::ArgAction::Set)]
-                    });
+                    // Insert at the front, so later (i.e. manually specified) clap attributes can
+                    // override it.
+                    field.attrs.insert(
+                        0,
+                        syn::parse_quote! {
+                            #[arg(
+                                num_args(0..=1),
+                                require_equals(true),
+                                action=clap::ArgAction::Set,
+                                // This is for when the flag is specified without any value (i.e. as
+                                // "--flag", not "--flag=whatever). Setting this to anything except True
+                                // would be super confusing, and it's not related to the default value.
+                                default_missing_value = "true",
+                            )]
+                        },
+                    );
 
-                    // Note that default_missing_value is for when the flag is specified without
-                    // any value (i.e. --flag, not --flag=whatever). Setting this to anything
-                    // except True would be super confusing.
                     if let Some(default) = default {
                         field.attrs.insert(
                             1,
                             syn::parse_quote! {
-                                #[arg(default_value_t = #default, default_missing_value = "true")]
-                            },
-                        );
-                    } else {
-                        field.attrs.insert(
-                            1,
-                            syn::parse_quote! {
-                                #[arg(default_missing_value = "true")]
+                                #[arg(default_value_t = #default)]
                             },
                         );
                     }
                 }
             }
         }
-        syn::Data::Enum(_) => todo!(),
-        syn::Data::Union(_) => todo!(),
+        Data::Enum(_) => {}
+        Data::Union(_) => {}
     }
     Ok(())
 }
 
-fn add_prefix_to_everything(prefix: &str, input: &mut DeriveInput) -> syn::Result<()> {
-    let mut name_style: fn(&str) -> String = <str as ToKebabCase>::to_kebab_case;
-
-    for attr in &input.attrs {
-        if let syn::Meta::List(list) = &attr.meta {
-            match list.path.get_ident() {
-                Some(id) if id == "clap" || id == "command" => (),
-                _ => continue,
-            }
-            let exprs =
-                Punctuated::<Expr, Token![,]>::parse_terminated.parse2(list.tokens.clone())?;
-            for expr in exprs {
-                match expr {
-                    Expr::Assign(expr)
-                        if expr.left.to_token_stream().to_string() == "rename_all" =>
-                    {
-                        let style = expr_literal(&expr.right)?;
-                        let mut norm = style.to_upper_camel_case().to_lowercase();
-                        norm = norm
-                            .strip_suffix("case")
-                            .map(ToOwned::to_owned)
-                            .unwrap_or(norm);
-                        name_style = match norm.as_str() {
-                            "pascal" => <str as ToPascalCase>::to_pascal_case,
-                            "kebab" => <str as ToKebabCase>::to_kebab_case,
-                            "camel" => <str as ToLowerCamelCase>::to_lower_camel_case,
-                            "screamingsnake" => <str as ToShoutySnakeCase>::to_shouty_snake_case,
-                            "snake" => <str as ToSnakeCase>::to_snake_case,
-                            "lower" => clap_to_lower,
-                            "upper" => clap_to_upper,
-                            "verbatim" => str::to_owned,
-                            _ => {
-                                return Err(Error::new_spanned(
-                                    expr.right,
-                                    "unrecognized rename style",
-                                ))
-                            }
-                        }
+fn get_bool_default(field: &Field) -> syn::Result<Option<bool>> {
+    let mut default = None;
+    for attr in &field.attrs {
+        if !attr.path().is_ident("arg") {
+            continue;
+        }
+        let exprs = attr.parse_args_with(Punctuated::<Expr, Token![,]>::parse_terminated)?;
+        for expr in exprs {
+            match expr {
+                Expr::Assign(expr) => match expr.left.to_token_stream().to_string().as_str() {
+                    "default_value" => {
+                        default = Some(expr_str_lit(&expr.right)? == "true");
                     }
+                    "default_value_t" => match expr.right.as_ref() {
+                        Expr::Lit(ExprLit {
+                            lit: Lit::Bool(LitBool { value, .. }),
+                            ..
+                        }) => {
+                            default = Some(*value);
+                        }
+                        _ => return Err(Error::new_spanned(expr.right, "expected a bool literal")),
+                    },
                     _ => (),
+                },
+                Expr::Call(call) if call.func.to_token_stream().to_string() == "default_value" => {
+                    let arg = call
+                        .args
+                        .first()
+                        .ok_or_else(|| Error::new_spanned(&call, "expected a single arg"))?;
+                    default = Some(expr_str_lit(arg)? == "true");
                 }
+                _ => (),
             }
         }
     }
+    Ok(default)
+}
+
+fn add_prefix_to_everything(prefix: &str, input: &mut DeriveInput) -> syn::Result<()> {
+    let name_style = get_field_name_style(input)?;
 
     match &mut input.data {
         syn::Data::Struct(DataStruct { fields, .. }) => {
@@ -262,7 +185,7 @@ fn add_prefix_to_everything(prefix: &str, input: &mut DeriveInput) -> syn::Resul
                                     Expr::Assign(expr)
                                         if expr.left.to_token_stream().to_string() == "long" =>
                                     {
-                                        let renamed = expr_literal(&expr.right)?;
+                                        let renamed = expr_str_lit(&expr.right)?;
                                         let name = format!("{prefix}.{renamed}");
                                         *expr = syn::parse_quote_spanned! { expr.span() => long = #name };
                                     }
@@ -273,7 +196,7 @@ fn add_prefix_to_everything(prefix: &str, input: &mut DeriveInput) -> syn::Resul
                                         match (expr.args.first(), expr.args.len()) {
                                             // long("foo")
                                             (Some(arg), 1) => {
-                                                let renamed = expr_literal(arg)?;
+                                                let renamed = expr_str_lit(arg)?;
                                                 let name = format!("{prefix}.{renamed}");
                                                 *expr = syn::parse_quote_spanned! { expr.span() => long(#name) };
                                             }
@@ -314,7 +237,55 @@ fn add_prefix_to_everything(prefix: &str, input: &mut DeriveInput) -> syn::Resul
     Ok(())
 }
 
-fn expr_literal(expr: &Expr) -> syn::Result<String> {
+fn get_field_name_style(input: &DeriveInput) -> syn::Result<fn(&str) -> String> {
+    let mut name_style: fn(&str) -> String = <str as ToKebabCase>::to_kebab_case;
+
+    for attr in &input.attrs {
+        if let syn::Meta::List(list) = &attr.meta {
+            match list.path.get_ident() {
+                Some(id) if id == "clap" || id == "command" => (),
+                _ => continue,
+            }
+            let exprs =
+                Punctuated::<Expr, Token![,]>::parse_terminated.parse2(list.tokens.clone())?;
+            for expr in exprs {
+                match expr {
+                    Expr::Assign(expr)
+                    if expr.left.to_token_stream().to_string() == "rename_all" =>
+                        {
+                            let style = expr_str_lit(&expr.right)?;
+                            let mut norm = style.to_upper_camel_case().to_lowercase();
+                            norm = norm
+                                .strip_suffix("case")
+                                .map(ToOwned::to_owned)
+                                .unwrap_or(norm);
+                            name_style = match norm.as_str() {
+                                "pascal" => <str as ToPascalCase>::to_pascal_case,
+                                "kebab" => <str as ToKebabCase>::to_kebab_case,
+                                "camel" => <str as ToLowerCamelCase>::to_lower_camel_case,
+                                "screamingsnake" => <str as ToShoutySnakeCase>::to_shouty_snake_case,
+                                "snake" => <str as ToSnakeCase>::to_snake_case,
+                                "lower" => clap_to_lower,
+                                "upper" => clap_to_upper,
+                                "verbatim" => str::to_owned,
+                                _ => {
+                                    return Err(Error::new_spanned(
+                                        expr.right,
+                                        "unrecognized rename style",
+                                    ))
+                                }
+                            }
+                        }
+                    _ => (),
+                }
+            }
+        }
+    }
+
+    Ok(name_style)
+}
+
+fn expr_str_lit(expr: &Expr) -> syn::Result<String> {
     let Expr::Lit(ExprLit {
         lit: Lit::Str(s), ..
     }) = expr
